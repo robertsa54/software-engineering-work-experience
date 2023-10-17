fa0574493408f73f77b9b75e14ea124f5b7c490e
robertsa54 <148204580+robertsa54@users.noreply.github.com>
2023-10-17 16:26:33 +0100

    Update challenge2.kt

--- src/main/kotlin/io/github/u/ways/challenge2.kt
+++ src/main/kotlin/io/github/u/ways/challenge2.kt
@@ -3,5 +3,7 @@ package io.github.u.ways
 import io.github.u.ways.domain.Request
 
 fun challenge2(request: Request) {
-
-}
+    if (!request.internet && !request.tv && !request.mobile && !request.landline) {
+      println("No products requested!")
+    }
+} 

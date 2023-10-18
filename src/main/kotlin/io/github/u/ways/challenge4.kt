8ba3887883e830de5158d4bdae29a5edf7c7d7d2
robertsa54 <148204580+robertsa54@users.noreply.github.com>
2023-10-18 16:30:15 +0100

    Update challenge4.kt

--- src/main/kotlin/io/github/u/ways/challenge4.kt
+++ src/main/kotlin/io/github/u/ways/challenge4.kt
@@ -3,4 +3,17 @@ package io.github.u.ways
 import io.github.u.ways.domain.Request
 
 fun challenge4(request: Request) {
-}
+    if (request.productCode==ProductCode.internet) {
+	    println ("Internet product code: F_004")    
+    }
+//     if (request.internet && request.mobile) {
+//         println ("Internet product code: F_004")
+//         println ("Mobile product code: F_002")
+//     }
+    if (request.productCode==ProductCode.landline) {
+        println ("Landline product code: F_001")
+    }
+//     if (!request.internet && !request.tv && !request.mobile && !request.landline) {
+//       println("No products requested!")
+//     }
+//     

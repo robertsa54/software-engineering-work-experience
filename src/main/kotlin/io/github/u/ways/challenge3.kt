c2ad305c019e81ed2a1c59bf5a1419bf276db23e
robertsa54 <148204580+robertsa54@users.noreply.github.com>
2023-10-18 14:13:53 +0100

    Update challenge3.kt

--- src/main/kotlin/io/github/u/ways/challenge3.kt
+++ src/main/kotlin/io/github/u/ways/challenge3.kt
@@ -3,4 +3,12 @@ package io.github.u.ways
 import io.github.u.ways.domain.Request
 
 fun challenge3(request: Request) {
+    if (request.name == "")
+    println("A mandatory field is missing!")
+    if (request.email == "")
+    println("A mandatory field is missing!")
+    if (request.phone == "")
+    println("A mandatory field is missing!")
+    if (request.address == "")
+    println("A mandatory field is missing!")
 }
